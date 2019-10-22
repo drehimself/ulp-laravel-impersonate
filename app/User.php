7990b2643f3ef4ddf1f7d6ce2d888b35e0455e99
Andre Madarang <andre_madarang@hotmail.com>
2019-10-22 18:29:28 -0400

    Working

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Lab404\Impersonate\Models\Impersonate;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Impersonate;
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +37,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // /**
+    //  * @return bool
+    //  */
+    // public function canImpersonate()
+    // {
+    //     return $this->id === 3;
+    // }
+
+    /**
+     * @return bool
+     */
+    public function canBeImpersonated()
+    {
+        return $this->id === 1;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
+Route::impersonate();
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/sensitive', 'HomeController@sensitive')->name('sensitive')->middleware('impersonate.protect');
+
+// Route::get('/imp', function () {
+//     $user = App\User::find(2);
+
+//     auth()->user()->impersonate($user);
+
+//     return view('home');
+// });
+
+// Route::get('/stop', function () {
+//     auth()->user()->leaveImpersonation();
+
+//     return view('home');
+// });
